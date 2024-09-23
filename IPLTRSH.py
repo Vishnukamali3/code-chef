959d8cf118537831d266c37d22f59f861f85a0a0
Vishnukamali3 <166210006+Vishnukamali3@users.noreply.github.com>
2024-09-23 22:31:40 +0530

    Add files via upload

---
+++ IPLTRSH.py
@@ -0,0 +1,10 @@
+# cook your dish here
+def cricket():
+    t=int(input())
+    for i in range(0,t):
+        n,m=map(int,input().split())
+        if n>m:
+            print(n-m)
+        else:
+            print("0")
+cricket()
