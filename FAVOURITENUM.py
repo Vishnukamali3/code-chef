f99e58ab74beff6d6d72a86aae921b69188b4109
Vishnukamali3 <166210006+Vishnukamali3@users.noreply.github.com>
2024-10-19 21:00:40 +0530

    Add files via upload

---
+++ FAVOURITENUM.py
@@ -0,0 +1,9 @@
+t = int(input())
+for i in range(t):
+    a=int(input())
+    if a%2==0 and a%7==0:
+        print("ALICE")
+    elif a%2!=0 and a%9==0:
+        print("BOB")
+    else:
+        print("CHARLIE")
