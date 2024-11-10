66d54e197d1ef42890bd80efb797b570fc0091d6
Vishnukamali3 <166210006+Vishnukamali3@users.noreply.github.com>
2024-11-10 20:37:38 +0530

    Add files via upload

--- FOODPLAN.py
+++ FOODPLAN.py
@@ -1,11 +1,12 @@
 # cook your dish here
-t = int(input())
+t=int(input())
 for i in range(t):
-    n,m = map(int, input().split())
-    z = n-n*0.1
-    if(z < m):
+    x,y=map(int,input().split())
+    a=(10/100)*x
+    b=x-a
+    if b<y:
         print("ONLINE")
-    elif(z > m):
-        print("DINING")
-    else:
+    elif b==y:
         print("EITHER")
+    else:
+        print("DINING")
